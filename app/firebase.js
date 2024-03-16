07773d2f138a21719482788a5f57415ae847dbd1
Doble-2 <angelcalderon303@gmail.com>
2024-03-16 01:47:18 -0400

    header footer and egg animation

--- app/firebase.js
+++ app/firebase.js
@@ -1,9 +1,14 @@
 // firebase.js
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
+import * as dotenv from "dotenv";
+dotenv.config();
+
+const apiKey= process.env.FIREBASE_API_KEY;
+
 
 const firebaseConfig = {
-    apiKey: "AIzaSyA2bnq2E2q9vI8nKK9fx4DdAl0irlX3CaQ",
+    apiKey:apiKey,
     authDomain: "nec-4bbf9.firebaseapp.com",
     projectId: "nec-4bbf9",
     storageBucket: "nec-4bbf9.appspot.com",
--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "./components/navbar";
+import Footer from "./components/footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,7 +13,15 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+     
+      <body className={inter.className}>
+       
+        <main className="flex min-h-screen flex-col items-center justify-between bg-bornCreme">
+        <Navbar/>
+          {children}
+        <Footer/>
+        </main>
+      </body>
     </html>
   );
 }
