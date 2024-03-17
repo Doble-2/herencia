504e56d72c845e72f3c849f962786b1a98224e4c
Kindex <miguelangelvera69@gmail.com>
2024-03-16 20:30:36 -0400

    di estilos a step hello y anadi input altura

--- app/steps/step_hello.js
+++ app/steps/step_hello.js
@@ -12,22 +12,26 @@ export  function StepHello() {
   const { handleNextStep, handlePreviousStep } = useFormStep()
 
   return (
-    <div className="px-8 py-12">
-    <div className="flex flex-col items-center">
-      <img
-        src="/placeholder.svg"
+    <div className="pr-44	pl-44	 py-12">
+    <div className="flex flex-row items-center">
+      
+      <div class="basis-1/2"><img
+        src=""
         alt="Two snowmen"
         className="mb-8"
         width="300"
         height="200"
         
-      />
-      <p className="text-center text-black mb-8">
+        
+      /></div>
+      <div class="basis-1/2">
+      <p className="text-center text-black mb-8 text-[#fff] ">
         ¡Hola! Soy Bory y cuando estaba en la barriga de mi mamá, mis padres tenían miedo de que sería mi condición
         genética, así que decidieron crear esta app y descubrieron que sería un patito feliz y sano y tu también
         puedes serlo. Con esta app puedes planificar tu embarazo con más tranquilidad así como lo hicieron mis
         papás.
       </p>
+      </div>
     </div>
     <button onClick={handlePreviousStep}>prev</button>
     <button onClick={handleNextStep}>sig</button>
